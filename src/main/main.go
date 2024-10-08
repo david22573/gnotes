8de2ac8056c194057a6f5088654b8f61fb0533b2
David Rivero <davidmiguel22573@gmail.com>
2024-10-07 18:11:59 -0700

    form example

--- src/main/main.go
+++ src/main/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Form Widget")
+	myWindow.Resize(fyne.NewSize(800, 600))
+	entry := widget.NewEntry()
+	textArea := widget.NewMultiLineEntry()
+
+	form := &widget.Form{
+		Items: []*widget.FormItem{ // we can specify items in the constructor
+			{Text: "Entry", Widget: entry}},
+		OnSubmit: func() { // optional, handle form submission
+			log.Println("Form submitted:", entry.Text)
+			log.Println("multiline:", textArea.Text)
+			myWindow.Close()
+		},
+	}
+
+	// we can also append items
+	form.Append("Text", textArea)
+
+	myWindow.SetContent(form)
+	myWindow.ShowAndRun()
 }
